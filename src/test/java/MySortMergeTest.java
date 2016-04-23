1b02b5da0ba989fef69e906a5be139b2437ac510
Vitalik <s.vitalii314@gmail.com>
2016-04-23 20:35:41 +0300

    Сортировка слиянием

--- src/test/java/MySortMergeTest.java
+++ src/test/java/MySortMergeTest.java
@@ -1,3 +1,10 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -6,4 +13,28 @@ import static org.junit.Assert.*;
 public class MySortMergeTest {
 
 
+
+    @Test
+    public void testSort() throws Exception {
+        int[] arr1 = new int[10];
+        int[] arr2 = new int[10];
+        for (int i =0 ; i<arr1.length; i++) {
+            arr1[i]=(int)(50*Math.random());
+        }
+        arr2=arr1;
+        MySortMerge.sort(arr1);
+        Arrays.sort(arr2);
+        assertTrue(arr1.equals(arr2));
+
+
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        JUnitCore runner = new JUnitCore();
+//        Result result = runner.run(MySortMergeTest.class);
+//        System.out.println("run tests: " + result.getRunCount());
+//        System.out.println("failed tests: " + result.getFailureCount());
+//        System.out.println("ignored tests: " + result.getIgnoreCount());
+//        System.out.println("success: " + result.wasSuccessful());
+//    }
 }
