95d97c30b2ed502f736aa7d6b570e3a62567c100
ValeriiKhudan <1860lucky1860@gmail.com>
2016-04-24 11:18:26 +0300

    test2

--- src/main/java/com/vertexacademy/hello/Sorter.java
+++ src/main/java/com/vertexacademy/hello/Sorter.java
@@ -4,8 +4,5 @@ package com.vertexacademy.hello;
  * Created by vertex0008 on 23.04.2016.
  */
 public interface Sorter {
-    //public void main();
-    //void heapSort();
-
     int[] doIncrediblyFastSort(int[] arr);
 }
