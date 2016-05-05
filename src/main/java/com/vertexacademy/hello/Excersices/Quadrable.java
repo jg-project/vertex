a20694accbd401aacdd38001112fba4633f9f057
Vasyl <leo124@bigmir.net>
2016-05-05 11:53:05 +0300

    My jdbc start

--- src/main/java/com/vertexacademy/hello/Excersices/Quadrable.java
+++
@@ -1,8 +0,0 @@
-package com.vertexacademy.hello.Excersices;
-
-/**
- * Created by user on 24.04.2016.
- */
-public interface Quadrable {
-    public  void countDouble(double a, double b, double c);
-}
